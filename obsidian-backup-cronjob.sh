4e784a033a49103f84bb79c7e5be535830987057
Tanmay Gupta <tg1482@stern.nyu.edu>
2025-01-02 10:04:46 -0800

    working with uncommited changes

--- obsidian-backup-cronjob.sh
+++ obsidian-backup-cronjob.sh
@@ -6,6 +6,15 @@ cd ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Obsidian\ Notes || e
 # Ensure the script runs with the repository
 git fetch origin
 
+# Add all changes and commit them if there are any
+git add .
+if git diff-index --quiet HEAD --; then
+    echo "$(date): No changes to commit." >> ~/dev/logs/cronlogs.txt
+else
+    git commit -m "Automatic backup $(date)"
+    echo "$(date): Changes committed." >> ~/dev/logs/cronlogs.txt
+fi
+
 # Compare local branch with the remote
 LOCAL=$(git rev-parse HEAD)
 REMOTE=$(git rev-parse @{u})
